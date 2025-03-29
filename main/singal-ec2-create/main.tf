c50e3e0b91da44d016f4a8d32af4e5bb6a150ee4
kusal-ec2 <kusal@gmail.com>
2025-03-29 09:06:00 +0000

    modify the repo

--- main/singal-ec2-create/main.tf
+++ main/singal-ec2-create/main.tf
@@ -1,20 +1,20 @@
 #call the ec2 module
 
 module "ec2_instance" {
-    source = "../modules/ec2"
-    ami_id = var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key_name
-    security_group_ids = var.security_group_ids
-    subnet_id = var.subnet_id
-    user_data = var.user_data
-    tags = var.tags
+  source             = "../../modules/ec2"
+  ami_id             = var.ami_id
+  instance_type      = var.instance_type
+  key_name           = var.key_name
+  security_group_ids = var.security_group_ids
+  subnet_id          = var.subnet_id
+  user_data          = var.user_data
+  tags               = var.tags
 }
 
 resource "local_file" "ec2_instance_details" {
-    content = <<EOF
+  content  = <<EOF
     EC2 Instance ID: ${module.ec2_instance.ec2_instance_id}
     EC2 Instance Public IP: ${module.ec2_instance.ec2_instance_public_ip}
     EOF
-    filename = "ec2_instance_details.txt"
-}
+  filename = "ec2_instance_details.txt"
+} 
--- main/singal-ec2-create/terraform.tfvars
+++ main/singal-ec2-create/terraform.tfvars
@@ -1,8 +1,8 @@
-ami_id = "ami-0f9de6e2d2f067fca"
-instance_type = "t2.micro"
-key_name = "kp-nv-nvms-test-stonebranch-01"
-security_group_ids = [ "sg-05a4afd7a6345e663" ]
-subnet_id = "subnet-0c64e9a918bdd8f28"
+ami_id             = "ami-0f9de6e2d2f067fca"
+instance_type      = "t2.micro"
+key_name           = "KP-NV-NVMS-TEST-StoneBranch-01"
+security_group_ids = ["sg-05a4afd7a6345e663"]
+subnet_id          = "subnet-0c64e9a918bdd8f28"
 
 user_data = <<EOF
 #!/bin/bash
@@ -11,8 +11,8 @@ sudo apt update -y 2>&1 >> /tmp/initial-setup.log
 EOF
 
 tags = {
-    "Name" = "ec2-module"
-    "Environment" = "dev"
+  "Name"        = "ec2-module"
+  "Environment" = "dev"
 }
 
 
